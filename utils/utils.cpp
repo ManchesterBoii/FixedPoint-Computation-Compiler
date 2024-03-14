106fe695beb3b009cbfe9f943f468b77cdd51e82
Nikolaus Dräger <nikolaus.draeger@tuwien.ac.at>
2024-03-14 01:57:22 -0700

    Implement calculation of number of bits

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,13 +1,24 @@
 #include <cmath>
 #include <iostream>
 
-int calcNumBits(int upperBound) {
-
-    if (upperBound <= 0) {
-        return 1;
+int calcNumBits(double lowerBound, double upperBound, int decimalBits) {
+    if (lowerBound > upperBound) {
+        throw std::invalid_argument("Lower bound should be less than or equal to upper bound.");
     }
 
-    int numBits = std::ceil(std::log2(static_cast<double>(upperBound) + 1));
+    int maxAbsValue = std::max(std::abs(lowerBound), std::abs(upperBound));
+    int bits = std::ceil(std::log2(maxAbsValue + 1)); 
 
-    return numBits;
+    // sign bit
+    bits++;
+
+    // precision bits
+    bits += decimalBits;
+
+    if (bits <= 8) return 8;
+    if (bits <= 16) return 16;
+    if (bits <= 32) return 32;
+    if (bits <= 64) return 64;
+
+    throw std::range_error("The number range is too large to be represented within 64 bits.");
 }
